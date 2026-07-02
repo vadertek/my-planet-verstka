287c14bde9333aa5bebbc7c56c23a58b5015b98e
Pavlo Leiko <drawnooblive@gmail.com>
2026-07-02 19:46:15 +0300

    NewSections

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -1,1 +1,33 @@
-﻿
+// In Trends mobile slider.
+let trendsSwiper = null;
+
+const initTrendsSwiper = (reset = false) => {
+    const isDesktop = window.matchMedia('(min-width: 768px)').matches;
+
+    if (isDesktop) {
+        trendsSwiper?.destroy(true, true);
+        trendsSwiper = null;
+        return;
+    }
+
+    if (!trendsSwiper) {
+        trendsSwiper = new Swiper('[data-trends-swiper]', {
+            slidesPerView: 1,
+            speed: 350,
+            watchOverflow: true,
+            navigation: {
+                prevEl: '[data-trends-swiper-prev]',
+                nextEl: '[data-trends-swiper-next]',
+            },
+        });
+    }
+
+    trendsSwiper.update();
+
+    if (reset) {
+        trendsSwiper.slideTo(0, 0);
+    }
+};
+
+window.updateTrendsSwiper = initTrendsSwiper;
+window.addEventListener('resize', () => initTrendsSwiper());
